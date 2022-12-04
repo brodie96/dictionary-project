a752a297eb92e315132f030175ac727fd5eb783a
brodie96 <101592243+brodie96@users.noreply.github.com>
2022-12-04 21:34:47 +0100

    added phonetics

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,9 +5,8 @@ import Examples from "./Examples";
 export default function Meaning(props) {
   return (
     <div className="Meaning">
-      <Synonyms synonyms={props.meaning.synonyms} />
       <h3>{props.meaning.partOfSpeech}</h3>
-
+      <Synonyms synonyms={props.meaning.synonyms} />
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
