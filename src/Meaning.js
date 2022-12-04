7eefcfacff34b12c9c00191e3898fdb1e7c970af
brodie96 <101592243+brodie96@users.noreply.github.com>
2022-12-04 21:07:31 +0100

    bug fix for examples

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,18 +1,19 @@
 import React from "react";
 import Synonyms from "./Synonyms";
+import Examples from "./Examples";
 
 export default function Meaning(props) {
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
       <Synonyms synonyms={props.meaning.synonyms} />
+      <h3>{props.meaning.partOfSpeech}</h3>
+
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
             <strong>Definition:</strong> {definition.definition}
             <br />
-            <strong>Example:</strong>
-            <em> {definition.example}</em>
+            <Examples examples={definition.example} />
             <br />
           </div>
         );
