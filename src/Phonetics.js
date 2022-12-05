81e92c92a06b2d204e5a49622f3ca173a9dabb86
brodie96 <101592243+brodie96@users.noreply.github.com>
2022-12-05 01:21:33 +0100

    added photos

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./Phonetics.css";
 
 export default function Phonetics(props) {
-  console.log(props);
   return (
     <div className="Phonetics">
       <span>{props.phonetic.text}</span>
